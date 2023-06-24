78b506d9c00bc72de517cd5a0aa85a37bf174ec5
Kieltatsu <kieltatsuno@gmail.com>
2023-06-24 17:29:22 -0300

    commit projeto

---
+++ mod5-2/PrimeiraClasse.java
@@ -0,0 +1,12 @@
+package br.com.kiel;
+
+public class PrimeiraClasse {
+
+	public static void main(String[] args) {
+		String ambiente = args [0];
+		
+				System.out.print("Hello Kiel");	
+
+		}
+	}
+
